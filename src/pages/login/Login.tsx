2d9ef5461d8c9826df809df20b330f1fa2507d5f
esteban93040 <espinosa.esteban@correounivalle.edu.co>
2025-04-20 16:50:04 -0500

    proyecto inicial

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import "./Login.css"
+
+
+const Login = () => {
+
+    
+
+    return( 
+        <div className="container-login">
+            <form className="login-form">
+                <img src="/public/svg/logos/loginBanner.svg" alt="logo" className="login-logo" />
+                <p> Username</p>
+                <input type="text" placeholder="Usuario" required />
+                <input type="password" placeholder="Contraseña" required />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login;

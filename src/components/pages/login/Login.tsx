f961de07c4c2f4f4975096601a9fe1cd75743114
Juan Miguel Rojas Noriega <juan.rojas@pragma.com.co>
2025-04-20 17:29:12 -0500

    Se ajusta imagen del public

--- src/components/pages/login/Login.tsx
+++ src/components/pages/login/Login.tsx
@@ -8,7 +8,7 @@ const Login = () => {
     return( 
         <div className="container-login">
             <form className="login-form">
-                <img src="/public/svg/logos/loginBanner.svg" alt="logo" className="login-logo" />
+                <img src="/svg/logos/loginBanner.svg" alt="logo" className="login-logo" />
                 <p> Username</p>
                 <input type="text" placeholder="Usuario" required />
                 <input type="password" placeholder="Contraseña" required />

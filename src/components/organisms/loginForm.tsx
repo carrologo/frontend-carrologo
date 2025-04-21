23db840fe42732ed177ae26cd126b5d68ee955c6
esteban93040 <espinosa.esteban@correounivalle.edu.co>
2025-04-20 19:55:18 -0500

    Aplicando metodologia atomic design

---
+++ src/components/organisms/loginForm.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TextField from '@mui/material/TextField'
+
+export default function loginForm () {
+  return ( 
+    <form>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </form>
+  )
+}
+
--- src/components/pages/login/Login.tsx
+++ src/components/pages/login/Login.tsx
@@ -1,21 +1,14 @@
-import "./Login.css"
-
+import React from 'react';
+import LoginForm from '../../organisms/loginForm';
 
 const Login = () => {
 
-    
-
-    return( 
-        <div className="container-login">
-            <form className="login-form">
-                <img src="/svg/logos/loginBanner.svg" alt="logo" className="login-logo" />
-                <p> Username</p>
-                <input type="text" placeholder="Usuario" required />
-                <input type="password" placeholder="Contraseña" required />
-                <button type="submit">Login</button>
-            </form>
-        </div>
-    )
+  return( 
+    <main className="login">
+      
+      <LoginForm/>
+    </main>
+  )
 }
 
 export default Login;

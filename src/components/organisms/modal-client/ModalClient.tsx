deb41e5aae7ac986472e27960e43568ce5fb6631
Juan Miguel Rojas Noriega <juan.rojas@pragma.com.co>
2025-04-21 22:05:23 -0500

    Se modifica la base para el Modal de Crear Cliente

--- src/components/organisms/modal-client/ModalClient.tsx
+++ src/components/organisms/modal-client/ModalClient.tsx
@@ -3,32 +3,35 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { Field, Form, Formik, FieldProps } from "formik";
 import dayjs from "dayjs";
-import { Box, Button, TextField } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import { DatePicker } from "@mui/x-date-pickers";
 
 const validationSchema = Yup.object({
   name: Yup.string().required("El nombre es obligatorio"),
   lastName: Yup.string().required("El apellido es obligatorio"),
+  identification: Yup.string(),
   birthdate: Yup.date()
     .required("La fecha de nacimiento es obligatoria")
     .nullable()
     .typeError("Ingrese una fecha válida"),
-  cellPhone: Yup.string()
+  contact: Yup.string()
     .matches(/^\+?\d{7,15}$/, "Ingrese un número de celular válido")
     .required("El celular es obligatorio"),
   email: Yup.string()
     .email("El correo electrónico no es válido")
     .required("El correo electrónico es obligatorio"),
+  comment: Yup.string(),
 });
 
 const initialValues = {
   name: "",
   lastName: "",
+  identification: "",
   birthdate: null,
-  cellPhone: "",
+  contact: "",
   email: "",
-  comments: "",
+  comment: "",
 };
 
 export const ModalClient = () => {
@@ -38,14 +41,15 @@ export const ModalClient = () => {
         initialValues={initialValues}
         validationSchema={validationSchema}
         onSubmit={(values, { setSubmitting }) => {
-          const { name, lastName, birthdate, cellPhone, email, comments } = values;
+          const { name, lastName, identification, birthdate, contact, email, comment } = values;
           const formattedValues = {
             name,
             lastName,
+            identification,
             birthdate: birthdate ? dayjs(birthdate).format("YYYY-MM-DD") : null,
-            cellPhone,
+            contact,
             email,
-            comments,
+            comment,
           };
           console.log("Formulario válido:", formattedValues);
           setSubmitting(false);
@@ -54,6 +58,7 @@ export const ModalClient = () => {
         {({ errors, touched, setFieldValue, isSubmitting, resetForm }) => (
           <Form>
             <Box sx={{ p: 5, bgcolor: "background.paper" }}>
+            <Typography variant="h4" color="text.primary" textAlign={"center"} mb={3} >Crear Cliente</Typography>
               <Grid container spacing={2}>
                 <Grid size={{xs: 12, md: 6}}>
                   <Field
@@ -80,6 +85,15 @@ export const ModalClient = () => {
                   />
                 </Grid>
                 <Grid size={{xs: 12, md: 6}}>
+                  <Field
+                    as={TextField}
+                    name="identification"
+                    label="Identificación"
+                    variant="outlined"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid size={{xs: 12, md: 6}}>
                   <Field name="birthdate">
                     {({ field }: FieldProps) => (
                       <DatePicker
@@ -94,13 +108,13 @@ export const ModalClient = () => {
                 <Grid size={{xs: 12, md: 6}}>
                   <Field
                     as={TextField}
-                    name="cellPhone"
+                    name="contact"
                     label="Celular"
                     variant="outlined"
                     required
                     fullWidth
-                    error={touched.cellPhone && Boolean(errors.cellPhone)}
-                    helperText={touched.cellPhone && errors.cellPhone}
+                    error={touched.contact && Boolean(errors.contact)}
+                    helperText={touched.contact && errors.contact}
                   />
                 </Grid>
                 <Grid size={{xs: 12, md: 6}}>
@@ -115,26 +129,15 @@ export const ModalClient = () => {
                     helperText={touched.email && errors.email}
                   />
                 </Grid>
-                <Grid size={{xs: 12, md: 6}}>
-                  <TextField
-                    label="Estado"
-                    value="Activo"
-                    variant="outlined"
-                    fullWidth
-                    disabled
-                  />
-                </Grid>
                 <Grid size={{xs: 12}}>
                   <Field
                     as={TextField}
-                    name="comments"
+                    name="comment"
                     label="Comentarios"
                     variant="outlined"
                     multiline
                     rows={4}
                     fullWidth
-                    error={touched.comments && Boolean(errors.comments)}
-                    helperText={touched.comments && errors.comments}
                   />
                 </Grid>
                 <Grid size={{ xs: 12 }}>

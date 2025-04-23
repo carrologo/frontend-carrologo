28f4dd2a68206277169e73e174e309a39ab5d53f
esteban93040 <espinosa.esteban@correounivalle.edu.co>
2025-04-22 22:58:48 -0500

    Creando tabla

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes,Route } from 'react-router-dom'
 import Login from './components/pages/login/Login'
 import './App.css'
+import Clients from './components/pages/clients/Clients'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/clientes" element={<Clients/>} />
       </Routes>
     </>
   )
---
+++ src/components/organisms/data-table/dataTable.tsx
@@ -0,0 +1,39 @@
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import Paper from '@mui/material/Paper';
+import './dataTable.css';
+
+const columns: GridColDef[] = [
+  { field: 'name', headerName: 'Nombre', width: 150 },
+  { field: 'lastName', headerName: 'Apellido', width: 150 },
+  { field: 'email', headerName: 'Correo', width: 300 },
+  { field: 'identification', headerName: 'Identificacion', width: 140,},
+  { field: 'birthdate', headerName: 'Nacimiento' , width: 140 },
+  { field: 'contact', headerName: 'Contacto', width: 150 },
+  { field: 'comment', headerName: 'Observaciones', width: 280 },
+  { field: 'edit', headerName: '', width: 60},
+  { field: 'delete', headerName: '', width: 60},
+
+
+];
+
+const rows = [
+  { id: 1, name: 'Juan Miguel', lastName: 'Rojas', email: 'juanmiguelrojasnoriega@hotmail.com', identification: 1143873302, birthdate: '1998-01-01', contact: 3141234567, comment: 'Sin observaciones' },
+];
+
+const paginationModel = { page: 0, pageSize: 5 };
+
+export default function DataTable() {
+  return (
+    <div className="datatable-container">
+    <Paper sx={{ height: '400px', width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{ pagination: { paginationModel } }}
+        pageSizeOptions={[5, 10]}
+        sx={{ border: 0, overflow: 'auto' }}
+      />
+    </Paper>
+    </div>
+  );
+}

3b119209197470e6059df2eb77126b37d511225e
Ans-Soft <anasoficantillo@gmail.com>
2025-04-23 01:28:01 -0500

    iconos, edit - delete

--- src/components/organisms/data-table/dataTable.tsx
+++ src/components/organisms/data-table/dataTable.tsx
@@ -1,6 +1,9 @@
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import Paper from '@mui/material/Paper';
 import './dataTable.css';
+import IconButton from '@mui/material/IconButton';
+import ModeEditIcon from '@mui/icons-material/ModeEdit'; 
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const columns: GridColDef[] = [
   { field: 'name', headerName: 'Nombre', width: 150 },
@@ -9,16 +12,54 @@ const columns: GridColDef[] = [
   { field: 'identification', headerName: 'Identificacion', width: 140,},
   { field: 'birthdate', headerName: 'Nacimiento' , width: 140 },
   { field: 'contact', headerName: 'Contacto', width: 150 },
-  { field: 'comment', headerName: 'Observaciones', width: 280 },
-  { field: 'edit', headerName: '', width: 60},
-  { field: 'delete', headerName: '', width: 60},
-
-
+  { field: 'comment', headerName: 'Observaciones', width: 230 },
+  {
+    field: 'edit',
+    headerName: '',
+    width: 60,
+    sortable: false,
+    filterable: false,
+    renderCell: (params) => (
+      <IconButton
+        aria-label="editar"
+        color="primary"
+        onClick={() => console.log('Editar', params.row)}
+      >
+        <ModeEditIcon />
+      </IconButton>
+    ),
+  },
+  {
+    field: 'delete',
+    headerName: '',
+    width: 60,
+    sortable: false,
+    filterable: false,
+    renderCell: (params) => (
+      <IconButton
+        aria-label="eliminar"
+        color="error"
+        onClick={() => console.log('Eliminar', params.row)}
+      >
+        <DeleteIcon />
+      </IconButton>
+    ),
+  },
 ];
 
 const rows = [
-  { id: 1, name: 'Juan Miguel', lastName: 'Rojas', email: 'juanmiguelrojasnoriega@hotmail.com', identification: 1143873302, birthdate: '1998-01-01', contact: 3141234567, comment: 'Sin observaciones' },
+  {
+    id: 1,
+    name: 'Juan Miguel',
+    lastName: 'Rojas',
+    email: 'juanmiguelrojasnoriega@hotmail.com',
+    identification: 1143873302,
+    birthdate: '1998-01-01',
+    contact: 3141234567,
+    comment: 'Sin observaciones',
+  },
 ];
+
 
 const paginationModel = { page: 0, pageSize: 5 };
 

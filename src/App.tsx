68de1ddd7d7b3b299b8a72bd04dfe2e4bc94be63
Juan Miguel Rojas Noriega <juan.rojas@pragma.com.co>
2025-04-23 15:42:57 -0500

    Se crean interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes,Route } from 'react-router-dom'
 import Login from './components/pages/login/Login'
 import './App.css'
 import { ModalCreateClient } from './components/templates/modal-create-client/ModalCreateClient'
+import Clients from './components/pages/clients/Clients'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<ModalCreateClient />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/clientes" element={<Clients />} />
       </Routes>
     </>
   )

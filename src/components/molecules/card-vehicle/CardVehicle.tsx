dcb474edf9e1e09b6c984dabab42877d33b7f8a7
jean palomino <118295259+jean-heyller@users.noreply.github.com>
2025-05-08 21:23:38 -0500

    Se actualiza el componente cardVehicle

--- src/components/molecules/card-vehicle/CardVehicle.tsx
+++ src/components/molecules/card-vehicle/CardVehicle.tsx
@@ -10,7 +10,6 @@ import {
 import Button from "@mui/material/Button";
 import { Vehicle } from "../../../interfaces/vehicles.interface";
 import { ModalEditVehicle } from "../../templates/modal-edit-vehicle/ModalEditVehicle";
-
 import ModeEditIcon from "@mui/icons-material/ModeEdit";
 
 interface CardVehicleProps {
@@ -18,46 +17,68 @@ interface CardVehicleProps {
 }
 
 const CardVehicle: React.FC<CardVehicleProps> = ({ vehicle }) => {
-  const [isModalOpen, setIsModalOpen] = React.useState(false);
+  const [openEdit, setOpenEdit] = useState(false);
 
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
-  };
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
+  const handleOpenEdit = () => setOpenEdit(true);
+  const handleCloseEdit = () => setOpenEdit(false);
 
   return (
-    <>
-      <Card>
-        <CardHeader
-          title={`${vehicle.brand} ${vehicle.line}`}
-          subheader={new Date(vehicle.model).getFullYear().toString()}
-        />
-        <CardMedia
-          component="img"
-          height="194"
-          image={vehicle.url_images || '/images/image-not-found.png'}
-          alt={vehicle.url_images || 'Vehicle'}
-        />
-        <CardContent>
-          <p><strong>Cilindraje:</strong> {vehicle.displacement} cc</p>
-          <p><strong>Transmisión:</strong> {vehicle.transmission}</p>
-          <p><strong>Kilometraje:</strong> {vehicle.kms.toLocaleString()} Km</p>
-          <p><strong>Tipo de Combustible :</strong> {vehicle.fuel_type}</p>
-        </CardContent>
-        <CardActions>
-          <Button size="small" onClick={handleOpenModal}>Ver detalles</Button>
-        </CardActions>
-      </Card>
+    <Card>
+      <CardHeader
+        title={`${vehicle.brand} ${vehicle.line}`}
+        subheader={new Date(vehicle.model).getFullYear().toString()}
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={vehicle.url_images || "/images/image-not-found.png"}
+        alt={vehicle.url_images || "Vehicle"}
+      />
+      <CardContent>
+        <p>
+          <strong>Cilindraje:</strong> {vehicle.displacement} cc
+        </p>
+        <p>
+          <strong>Transmisión:</strong> {vehicle.transmission}
+        </p>
+        <p>
+          <strong>Kilometraje:</strong> {vehicle.kms.toLocaleString()} Km
+        </p>
+        <p>
+          <strong>Tipo de Combustible :</strong> {vehicle.fuel_type}
+        </p>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Ver detalles</Button>
+        <Button
+          size="small"
+          onClick={handleOpenEdit}
+          startIcon={<ModeEditIcon />}
+        >
+          Editar
+        </Button>
+      </CardActions>
+      <Dialog open={openEdit} maxWidth="md" fullWidth>
+  <ModalEditVehicle
+    onClose={handleCloseEdit}
+    vehicleId={vehicle.id}
+    initialData={{
+      ...vehicle,
+      images: vehicle.url_images
+        ? [
+            {
+              base64: vehicle.url_images,
+              name: "imagen-vehiculo.jpg",
+              type: "image/jpeg",
+            },
+          ]
+        : [],
+    }}
+    onVehicleEdited={handleCloseEdit}
+  />
+</Dialog>
 
-      <Dialog open={isModalOpen} onClose={handleCloseModal} maxWidth="md" fullWidth>
-        <ModalViewVehicle
-          onClose={handleCloseModal}
-          initialValues={vehicle} // Pasa los datos del vehículo al modal
-        />
-      </Dialog>
-    </>
+    </Card>
   );
 };
 
